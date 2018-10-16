7f15bd42e02c90b10013bd0f4bb4204e57fe1a59
Quinn Z <Quinn Z>
2018-10-16 16:55:16 -0500

    New version of code with next assign statements

--- part_B/display.v
+++ part_B/display.v
@@ -13,39 +13,47 @@ module display
     reg [9:0] hSyncCounter = 0;
     reg [9:0] vSyncCounter = 0;
     
+    wire [9:0] hSyncCounter_next = 0;
+    wire [9:0] vSyncCounter_next = 0;
+    
+    assign hSyncCounter_next = (hSyncCounter == 799) ? 0 : hSyncCounter + 1;
+    assign vSyncCounter_next = (hSyncCounter == 799) ? ((vSyncCounter == 799) ? 0 : vSyncCounter + 1) : vSyncCounter;
+
     always @(posedge clk25) begin
-        if (((hSyncCounter >= 639) && (hSyncCounter < 799)) || ((vSyncCounter >= 480) && (hSyncCounter < 639)) || ((vSyncCounter == 479)
-                                            && (hSyncCounter == 799)))
+        
+        if ((hSyncCounter_next >= 640) || (vSyncCounter_next >= 480))
         begin
             red_out     <= 4'h0;
             blue_out    <= 4'h0;
             green_out   <= 4'h0;
         end
         else
-        begin  
+        begin
             red_out     <= 4'hF;
             blue_out    <= 4'hF;
             green_out   <= 4'hF;
         end
         
-        
-        if ((hSyncCounter >= 658) && (hSyncCounter < 755)) begin
+        if ((hSyncCounter_next >= 659) && (hSyncCounter_next <= 755))
+        begin
             hSync <= 0;
         end
-        else begin
+        else
+        begin
             hSync <= 1;
         end
         
-        if (((vSyncCounter == 492) && (hSyncCounter == 799)) || ((vSyncCounter > 492) && ((vSyncCounter < 494) && (hSyncCounter < 799)))) begin
+        if ((vSyncCounter_next == 493) || (vSyncCounter_next == 494))
+        begin
             vSync <= 0;
         end
-        else begin
+        else
+        begin
             vSync <= 1;
         end
-    
-        hSyncCounter <= (hSyncCounter == 799) ? 0 : hSyncCounter + 1;
-        if (hSyncCounter == 799) begin
-            vSyncCounter <= (vSyncCounter == 524) ? 0 : vSyncCounter + 1;
-        end
+        
+        
+        hSyncCounter <= hSyncCounter_next;
+        vSyncCounter <= vSyncCounter_next;    
     end
 endmodule
