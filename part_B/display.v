aaafffb201f6562ef5b8d871ac0ccdab99370e30
Quinn Z <quinn95@gmail.com>
2018-10-17 22:01:25 -0500

    Bouncing square

--- part_B/display.v
+++ part_B/display.v
@@ -1,8 +1,10 @@
-
+`define RED     11:8
+`define GREEN   7:4
+`define BLUE    3:0
 
 module display
     ( input clk25
-    , input [11:0] rbg
+    , input [11:0] rgb
     , output reg [3:0] red_out
     , output reg [3:0] blue_out
     , output reg [3:0] green_out
@@ -16,11 +18,12 @@ module display
     reg [9:0] hSyncCounter_next = 0;
     reg [9:0] vSyncCounter_next = 0;
     
-
-
-    always @(posedge clk25) begin
+    always @(hSyncCounter or vSyncCounter) begin
         hSyncCounter_next = (hSyncCounter == 799) ? 0 : hSyncCounter + 1;
         vSyncCounter_next = (hSyncCounter == 799) ? ((vSyncCounter == 524) ? 0 : vSyncCounter + 1) : vSyncCounter;
+    end
+
+    always @(posedge clk25) begin
 
         if ((hSyncCounter_next >= 640) || (vSyncCounter_next >= 480))
         begin
@@ -30,9 +33,9 @@ module display
         end
         else
         begin
-            red_out     <= 4'h0;
-            blue_out    <= 4'h0;
-            green_out   <= 4'hF;
+            red_out     <= rgb[`RED];
+            blue_out    <= rgb[`BLUE];
+            green_out   <= rgb[`GREEN];
         end
         
         if ((hSyncCounter_next >= 659) && (hSyncCounter_next <= 755))
--- part_B/display_top.v
+++ part_B/display_top.v
@@ -8,11 +8,56 @@ module display_top(
     , output Hsync
     , output Vsync
 );
-    reg [11:0] rgb;
+    reg [11:0] rgb = 0;
     wire clk25;
     
+    reg [11:0] x = 0;
+    reg [11:0] y = 0;
+    
+    reg dir = 0;
+    reg [12:0] pos = 0;
+    reg [3:0] delta = 1;
+    
+    reg [15:0] frame = 0;
+
+    always@(posedge clk25) begin
+        frame <= frame + 1;
+    end
     clkdiv25 divider(clk, clk25);
     
     display display(clk25, rgb, vgaRed, vgaBlue, vgaGreen, Hsync, Vsync);
+
+    always @(posedge clk25) begin
+        
+        x <= (x == 799) ? 0 : x + 1;
+        y <= (x == 799) ? ((y == 524) ? 0 : y + 1) : y;
+        if (((x > (pos) && (x < pos + 80))
+            && ((y > 200) && (y < 280))))
+            rgb <= 12'h00F;
+        else
+            rgb <= 12'hF00;
+    end
+    
+    always @(posedge frame[15]) begin
+    if (dir == 0) begin
+        if (pos == (640 - 80)) begin
+            dir <= 1;
+            pos <= pos - delta;
+        end
+        else begin
+            pos <= pos + delta;
+        end
+    end
+            else begin
+        if (pos == 0) begin
+            dir <= 0;
+            pos <= pos + delta;
+        end
+        else begin
+            pos <= pos - delta;
+        end
+    end
+
+    end
 
 endmodule
