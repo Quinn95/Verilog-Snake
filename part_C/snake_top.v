bdc826558b013026241e0c11a66517c8c85cd251
Quinn Z <quinn95@gmail.com>
2018-10-18 16:51:15 -0500

    Added boilerplate, hooked up display and keyboard driver to top

--- part_C/snake_top.v
+++ part_C/snake_top.v
@@ -10,17 +10,32 @@ module snake_top
     , output Hsync
     , output Vsync
     );
+    
+    wire clk25;
 
-wire died; // 1 if the snake just died
-wire init_snake; //1 if the snake is to be set to init value
-wire screen_black; //1 if screen is set all black
-wire screen_pause; //1 for freeze (dead or pause) and 0 for running snake
+    wire died; // 1 if the snake just died
+    wire init_snake; //1 if the snake is to be set to init value
+    wire screen_black; //1 if screen is set all black
+    wire screen_pause; //1 for freeze (dead or pause) and 0 for running snake
+
+    reg [11:0] rgb;
+    
+    wire key_pressed; // turns high for one ps2 clock cycle when a key is pressed
+    reg [7:0] key_code; // the 8 bit code of the last key pressed
 
 
+    display display(.clk25(clk25), .rgb(rgb), .red_out(vgaRed),
+                    .blue_out(vgaBlue), .green_out(vgaGreen),
+                    .hSync(Hsync), .vSync(Vsync));
 
+    ps2 keyboard(.clk(PS2Clk), .data(PS2Data), .key_pressed(key_pressed),
+                 .last_pressed(key_code));
+    
+    //FSM that determines if anything is ouput on screen
+    game_state gs(clk, died, key_code, init_snake, screen_black, screen_pause);
 
+    always @(posedge clk) begin
 
-//FSM that determines if anything is ouput on screen
-game_state gs(clk, died, key_code, init_snake, screen_black, screen_pause);
+    end
 
 endmodule
