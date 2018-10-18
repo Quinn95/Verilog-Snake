9b09227748618d4b1cde36043b11b9a9e8abefed
Quinn Z <quinn95@gmail.com>
2018-10-18 17:24:49 -0500

    Added square again... Square moves left or right depending on last pressed arrow key

--- part_C/snake_top.v
+++ part_C/snake_top.v
@@ -21,7 +21,7 @@ module snake_top
     reg [11:0] rgb;
     
     wire key_pressed; // turns high for one ps2 clock cycle when a key is pressed
-    reg [7:0] key_code; // the 8 bit code of the last key pressed
+    wire [7:0] key_code; // the 8 bit code of the last key pressed
 
     clkdiv25 cd25 (clk, clk25);
 
@@ -33,10 +33,39 @@ module snake_top
                  .last_pressed(key_code));
     
     //FSM that determines if anything is ouput on screen
-    game_state gs(clk, died, key_code, init_snake, screen_black, screen_pause);
+    //game_state gs(clk, died, key_code, init_snake, screen_black, screen_pause);
 
-    always @(posedge clk) begin
-
+    reg [11:0] x = 0, y = 0;
+    reg [12:0] pos = 0;
+    reg [3:0] delta = 5;
+        
+    always @(posedge clk25) begin
+        x <= (x == 799) ? 0 : x + 1;
+        y <= (x == 799) ? ((y == 524) ? 0 : y + 1) : y;
+        if (((x > (pos) && (x < pos + 80))
+            && ((y > 200) && (y < 280))))
+            rgb <= 12'h00F;
+        else
+            rgb <= 12'hF00;
+    end
+    
+    always @(negedge Vsync) begin
+    if (key_code == 8'h74) begin
+        if (pos == (640 - 80)) begin
+            pos <= pos;
+        end
+        else begin
+            pos <= pos + delta;
+        end
+    end
+    else if (key_code == 8'h6B) begin
+        if (pos == 0) begin
+            pos <= pos;
+        end
+        else begin
+            pos <= pos - delta;
+        end
+    end
     end
 
 endmodule
