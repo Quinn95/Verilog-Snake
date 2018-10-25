b2d8361572b4fe6fe480d2048c8440fcacea7c08
Raphaela <eberliraphaela@gmail.com>
2018-10-25 17:41:29 -0500

    segment display

---
+++ part_C/segdisplay_driver.v
@@ -0,0 +1,29 @@
+
+
+module segdisplay_driver
+    ( input  clk
+    , input [15:0] num
+    , input enable
+    , output [3:0] an
+    , output [6:0] seg_out
+    );
+    
+    wire [3:0] num0, num1, num2, num3;
+    
+    wire [6:0] num0_7seg;
+    wire [6:0] num1_7seg;
+    wire [6:0] num2_7seg;
+    wire [6:0] num3_7seg;
+    
+    decimal2bcd d2bcd(num, num0, num1, num2, num3);
+   
+    hexto7segment seg0(num0, num0_7seg);
+    hexto7segment seg1(num1, num1_7seg);
+    hexto7segment seg2(num2, num2_7seg);
+    hexto7segment seg3(num3, num3_7seg);
+
+    segdisplay_multiplexer plex0(clk,
+        num0_7seg, num1_7seg, num2_7seg, num3_7seg, enable,
+        an, seg_out);
+    
+endmodule
---
+++ part_C/segdisplay_multiplexer.v
@@ -0,0 +1,61 @@
+
+/*
+    num[0,1,2,3] is the number to display for each digit, 
+    starting with the far right (0).
+    
+    an is the position of the current digit to be displayed
+    (the board only displays one at a time).  
+*/
+
+module segdisplay_multiplexer
+    ( input clk
+    , input [6:0] num0
+    , input [6:0] num1
+    , input [6:0] num2
+    , input [6:0] num3
+    , input enable
+    , output reg [3:0] an
+    , output reg [6:0] seg_out
+    );
+    
+    reg [10:0] slow_clk;
+    
+    always@(posedge clk) begin
+        slow_clk <= slow_clk + 1;
+    end
+    
+    initial begin
+        an = 4'b1110;
+        seg_out = 7'b0000000;
+    end
+
+    always@(posedge slow_clk[10]) begin
+        if (enable) begin
+            if(an == 4'b1110) begin
+                an <= 4'b1101;
+                seg_out <= num1;
+            end
+            else if(an == 4'b1101) begin
+                an <= 4'b1011;
+                seg_out <= num2;
+            end
+            else if(an == 4'b1011) begin
+                an <= 4'b0111;
+                seg_out <= num3;
+            end
+            else if(an == 4'b0111) begin
+                an <= 4'b1110;
+                seg_out <= num0;
+            end
+            else begin
+                an <= 4'b1110;
+                seg_out <= 4'b1111;
+            end
+        end
+        else begin
+            an <= 4'b1111;
+            seg_out <= 4'b1111;
+        end       
+    end
+
+endmodule
